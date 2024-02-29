ebb9ae3dc51ff31b11af0b4c674a233f4d48d1eb
Abdülbaki Öztürk <aozturk@nuevo.com.tr>
2024-03-01 00:20:44 +0300

    Add project files.

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace EventBus.Base.Events
+{
+    public class IntegrationEvent
+    {
+
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+        }
+
+        [JsonConstructor]
+        public IntegrationEvent(Guid id, DateTime createdDate)
+        {
+            Id = id;
+            CreatedDate = createdDate;
+        }
+
+        [JsonProperty]
+        public Guid Id { get; private set; }
+        public DateTime CreatedDate { get; private set; }
+    }
+}
